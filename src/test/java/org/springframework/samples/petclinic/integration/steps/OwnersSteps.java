d24436dbb11235fe0cacc5bdbd6551065d10dbc5
belousov <belousov.nikolai@gmail.com>
2021-04-29 11:09:45 +0300

    Added 'find owner' TCs

--- src/test/java/org/springframework/samples/petclinic/integration/steps/OwnersSteps.java
+++ src/test/java/org/springframework/samples/petclinic/integration/steps/OwnersSteps.java
@@ -1,11 +1,14 @@
 package org.springframework.samples.petclinic.integration.steps;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.samples.petclinic.integration.utils.LocatorParser.*;
 import static org.springframework.samples.petclinic.integration.utils.SelenideTools.openUrl;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import io.cucumber.java.en.Given;
@@ -13,6 +16,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.samples.petclinic.integration.Constants;
 import org.springframework.samples.petclinic.integration.SpringIntegrationTest;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public class OwnersSteps extends SpringIntegrationTest {
 
@@ -48,7 +54,31 @@ public class OwnersSteps extends SpringIntegrationTest {
 	public void shouldSeeThePage(String pageTitle) {
 		By elementSelector = By.tagName("h2");
 		String text = $(elementSelector).shouldBe(Condition.visible).getText();
+
 		assertThat(text).isEqualTo(pageTitle);
+	}
+
+	@Then("^I should see results relative to \"([^\"]*)\" query$")
+	public void searchResultsAreRelativeToQuery(String query) {
+		By elementSelector = By.xpath("//table[@id='owners']/tbody/tr/td[1]/a");
+		List<String> texts = $$(elementSelector).texts();
+
+		Stream.of(texts).forEach(name -> assertThat(name.contains(query)));
+	}
+
+	@Then("^I should see all existing users found$")
+	public void shouldSeeAllExistingUsersFound() {
+		By elementSelector = By.xpath("//table[@id='owners']/tbody/tr/td[1]/a");
+		List<String> texts = $$(elementSelector).texts();
+
+		assertThat(texts.size() > 0);
+	}
+
+	@Then("^I should see 'has not been found' alert$")
+	public void shouldSeeHasNotBeenFoundAlert() {
+		By elementSelector = By.xpath("//div[@id='lastNameGroup']//div/p");
+
+		assertThat($(elementSelector).shouldBe(Condition.visible).isDisplayed());
 	}
 
 }
--- src/test/resources/scenarios/FindOwners.feature
+++ src/test/resources/scenarios/FindOwners.feature
@@ -16,3 +16,32 @@ Feature: Find owners
     When I fill the field named "lastName" with value "Davis"
     And I submit the form "search-owner-form"
     Then I should see the "Owners" page
+    And I should see results relative to "Davis" query
+
+  Scenario: Should find all owners
+    Given I go to the find-owners page
+    When I fill the field named "lastName" with value ""
+    And I submit the form "search-owner-form"
+    Then I should see the "Owners" page
+    Then I should see all existing users found
+
+  Scenario: Should find all owners with last name staring of the same letters
+    Given I go to the find-owners page
+    When I fill the field named "lastName" with value "Es"
+    And I submit the form "search-owner-form"
+    Then I should see the "Owners" page
+    And I should see results relative to "Es" query
+
+  Scenario: Should not find owner by first name
+    Given I go to the find-owners page
+    When I fill the field named "lastName" with value "George"
+    And I submit the form "search-owner-form"
+    Then I should see the "Find Owners" page
+    Then I should see 'has not been found' alert
+
+  Scenario: Should not find owner by pet name
+    Given I go to the find-owners page
+    When I fill the field named "lastName" with value "Leo"
+    And I submit the form "search-owner-form"
+    Then I should see the "Find Owners" page
+    Then I should see 'has not been found' alert
